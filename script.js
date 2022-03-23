bc032ab59967c1e81bad27df9f7381088f9fecf7
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-03-23 14:09:36 +0100

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const commands = ["git config -l", ""];
+const commands = [
+  "git config -l",
+  "git push",
+  "git commit -m 'message'",
+  "git pull",
+  "git log",
+  "git branch",
+  "git checkout",
+  "git init",
+  "git status",
+  "git add",
+];
+
+const titles = document.getElementById("list-example");
+const content = document.getElementById("text-container");
+
+for (let index = 0; index < commands.length; index++) {
+  titles.innerHTML += `<h4 id="list-item-1">${commands[index]}</h4>
+<p>...</p>`;
+}
